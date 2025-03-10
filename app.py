c046d17cb0cfe82a71852486b4d3f964e5facc39
Nom075 <graytonl@nycstudents.net>
2025-03-10 10:17:23 -0400

    more hard stuff

--- app.py
+++ app.py
@@ -14,14 +14,33 @@ def csv_to_list(file_path):
 file_path = "SalesData.csv"  
 data = csv_to_list(file_path)
 
+#Assignment 1
+""" def average(data):
+    sum = 0
+    for rows in data[1:]:
+        for index, number in enumerate(rows[1:], start = 1):
+            sum = sum + int(number)
+        print(f"The average sales for {rows[0]} is {round(sum/index)}")
+average(data)
+ """
 
+#assignment 2
 def average(data):
-    for rows in data:
-        print(rows[1:] if number.isdigit() else number for number in rows)
+    sorted = []
+    for rows in data[1:]:
+        sum = 0
+        average = 0
+        for index, number in enumerate(rows[1:], start = 1):
+            sum = sum + int(number)
+
+        average = round(sum/index)
+        print(f"The average sales for {rows[0]} is {average}")
+        for index in enumerate[sorted]:
+            if average > sorted[index]:
+                sorted.insert(index, average)
+        print(sorted)
+        
 
 average(data)
 
-
-
     
-
