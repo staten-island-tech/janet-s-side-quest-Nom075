32a63867b2d70b745e7fd4132355d18744bc1832
Nom075 <graytonl@nycstudents.net>
2025-03-11 10:16:17 -0400

    Final app

--- app.py
+++ app.py
@@ -16,31 +16,101 @@ data = csv_to_list(file_path)
 
 #Assignment 1
 """ def average(data):
-    sum = 0
+    total = 0
     for rows in data[1:]:
         for index, number in enumerate(rows[1:], start = 1):
-            sum = sum + int(number)
-        print(f"The average sales for {rows[0]} is {round(sum/index)}")
+            total = total + int(number)
+        print(f"The average sales for {rows[0]} is {round(total/index)}")
 average(data)
  """
 
 #assignment 2
-def average(data):
+""" def average(data):
     sorted = []
     for rows in data[1:]:
-        sum = 0
+        total = 0
         average = 0
         for index, number in enumerate(rows[1:], start = 1):
-            sum = sum + int(number)
+            total = total + int(number)
 
-        average = round(sum/index)
-        print(f"The average sales for {rows[0]} is {average}")
-        for index in enumerate[sorted]:
-            if average > sorted[index]:
-                sorted.insert(index, average)
+        average = round(total/index)
+        store_name = rows[0]
+        sorted.append((store_name, average))
+        sorted.sort(key = lambda x: x[1])
+
         print(sorted)
         
 
-average(data)
+average(data) """
 
-    
+#assignment 3   
+""" def average(data):
+    sorted = []
+    for rows in data[1:]:
+        total = 0
+        average = 0
+        for index, number in enumerate(rows[1:], start = 1):
+            total += int(number)
+
+        average = round(total/index)
+        store_name = rows[0]
+        sorted.append((store_name, average))
+        sorted.sort(key = lambda x: x[1])
+
+    print(sorted)
+    all_average = sum(x[1] for x in sorted)/len(sorted)
+    print(all_average)
+        
+
+average(data)
+ """
+
+""" #assignment 4
+def average(data):
+    sorted = []
+    for rows in data[1:]:
+        total = 0
+        average = 0
+        for index, number in enumerate(rows[1:], start = 1):
+            total += int(number)
+
+        average = round(total/index)
+        store_name = rows[0]
+        sorted.append((store_name, average))
+        sorted.sort(key = lambda x: x[1])
+
+    print(sorted)
+    all_average = sum(x[1] for x in sorted)/len(sorted)
+    print(f"All store average: {all_average}")
+    for value in sorted:
+        if value[1] < 0.8*all_average:
+            print(f"{value[0]} is in danger of being closed.")
+
+        
+
+average(data) """
+
+#master function
+def average(data):
+    sorted = []
+    for rows in data[1:]:
+        total = 0
+        average = 0
+        for index, number in enumerate(rows[1:], start = 1):
+            total += int(number)
+
+        average = round(total/index)
+        store_name = rows[0]
+        sorted.append((store_name, average))
+        sorted.sort(key = lambda x: x[1])
+
+    print(sorted)
+    all_average = sum(x[1] for x in sorted)/len(sorted)
+    print(f"All store average: {all_average}")
+    for value in sorted:
+        if value[1] < 0.8*all_average:
+            print(f"{value[0]} is in danger of being closed.")
+
+        
+
+average(data)
